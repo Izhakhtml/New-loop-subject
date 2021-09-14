d68d9917c6d7ae0af0a8c5ed4fe90ca95efe6d08
Izhakhtml <Izhaklijalem28@gmail.com>
2021-09-14 21:58:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+//.................... exe 1
+// let articleArray =
+// [
+//  {title:"aaa",ahouthr:"shay",year:2000},
+//  {title:"bbb",ahouthr:"izhak",year:2012},
+//  {title:"ccc",ahouthr:"sharon",year:2014},
+// ]
+//............... exe .a
+// articleArray.forEach((element) => {
+//     console.log(element);
+// });
+//............... exe .b
+// for (const iterator of articleArray) {
+//     console.log(iterator);
+// }
+
+//.................... exe 2
+// for (const key in articleArray) {
+//    console.log(articleArray[key]);
+// }
+
+//.................... exe 3
+// let books =
+// [
+//  {name:"aaa",ahouthr:"shay",price:2000},
+//  {name:"bbb",ahouthr:"izhak",price:2012},
+//  {name:"ccc",ahouthr:"sharon",price:2014},
+// ]
+//............... exe .a
+// for (const key in books) {
+//    console.log(books[key]);
+// }
+//............... exe .b
+// for (const iterator of books) {
+//     console.log(iterator.name);
+// }
+
+//.................... exe 4
+// const array = [1,2,3,4,5,6,7,8,9]
+// let sum = 0
+//............... exe .a
+// array.forEach((element) => {
+//    console.log(sum+=element  ); 
+// });
+//............... exe .b
+// for (const number of array) {
+//     console.log(sum+=number);
+// }
